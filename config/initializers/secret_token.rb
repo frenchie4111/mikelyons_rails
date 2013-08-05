99968c4f664dbf4ed9675c266bbde3770705b733
Mike Lyons <mdl0394@gmail.com>
2013-08-04 22:07:20 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mikelyons::Application.config.secret_key_base = 'eb7196f72b585060c468aa5f9b8e8345580951162a4eed5aa7a640994e15a5097cc3e30439c6efedb45f23df218a0b822a0a0ceb589a326cb702a5ec64b81e9d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mikelyons::Application.config.session_store :cookie_store, key: '_mikelyons_session'
